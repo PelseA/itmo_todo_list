c6901b74e1cf1f6ec8829c00009e3710f7a55ddd
Aleksandra Pelse <shashka_aya@mail.ru>
2022-11-07 22:34:18 +0300

    add package.json

---
+++ spectrum.js
@@ -0,0 +1,3 @@
+import '@spectrum-web-components/bundle/elements.js';
+import '@spectrum-web-components/button';
+import '@spectrum-web-components/textfield';
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+    // config options
+    server: {
+        port: 8081
+    }
+
+}
