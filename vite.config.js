e1da0767fc565a90aa13fc0d2adac038e2e66831
Aleksandra Pelse <shashka_aya@mail.ru>
2022-11-10 22:09:42 +0300

    add tests

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-// vite.config.js
-export default {
-    // config options
-    server: {
-        port: 8081
-    }
+// vite.config.js/** @type {import('vite').UserConfig} */
+import path from 'path';
+import { defineConfig } from 'vite';
+// import mkcert from 'vite-plugin-mkcert';
+console.log(import.meta.env)
 
-}
+export default defineConfig({
+    // omit
+    plugins: [],
+    resolve: {
+        alias: {
+            '@/': `${path.resolve(__dirname, 'src')}/`,
+        },
+    },
+    server: {
+        host: '127.0.0.1',
+        port: 8081, // cors: true,    // host: 'local.dev',    // https: true,
+    },
+});
